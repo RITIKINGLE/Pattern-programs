1827ee812d953de2615a2959c3b39d4c5aaff54f
RITIKINGLE <78408525+RITIKINGLE@users.noreply.github.com>
2023-08-17 22:18:24 +0530

    Add files via upload

---
+++ problem106.java
@@ -0,0 +1,30 @@
+/*
+ABCDE
+BCDE
+CDE
+DE
+E
+DE
+CDE
+BCDE
+ABCDE
+*/
+
+public class problem106{
+	public static void main(String args[]){
+		int n=5;
+		for(int i=1; i<=(n*2)-1; i++)
+		{
+			if(i<=n){
+				for(int j=i; j<=n; j++)
+				System.out.print((char)(j+64));
+				
+			}
+			else{
+				for(int j=(n*2)-i; j<=n; j++)
+				System.out.print((char)(j+64));
+			}
+			System.out.println();
+		}
+	}
+}
---
+++ problem110.java
@@ -0,0 +1,30 @@
+/*
+A
+BA
+CBA
+DCBA
+EDCBA
+DCBA
+CBA
+BA
+A
+*/
+
+public class problem110{
+	public static void main(String args[]){
+		int n=5;
+		for(int i=1; i<=(n*2)-1; i++)
+		{
+			if(i<=n){
+				  for(int j=i; j>=1; j--)
+				   System.out.print((char)(j+64));
+				} 
+			else{
+				for(int j=(n*2)-i;j>=1;j--)
+				 System.out.print((char)(j+64));
+			}
+		System.out.println();
+		}
+		
+	}		
+}
