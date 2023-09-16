601ecbaac84b1547e1e780ba714922e6fe914847
RITIKINGLE <78408525+RITIKINGLE@users.noreply.github.com>
2023-09-16 10:32:45 +0530

    num pattern

---
+++ problem218.java
@@ -0,0 +1,33 @@
+/*
+543212345
+5432 2345
+543   345
+54     45
+5       5
+*/
+
+public class program218{
+	public static void main(String args[]){
+		int n=9;
+		for(int i=1; i<=n; i++){
+		if(i==1){
+			for(int j=n; j>=i; j--)
+			   System.out.print(j);
+
+			for(int j=i+1; j<=n; j++)
+			   System.out.print(j);			
+			}
+			else{
+				for(int j=n; j>=i; j--)
+				   System.out.print(j);
+
+				for(int j=1; j<=((i-1)*2)-1; j++)
+				   System.out.print(" ");
+
+				for(int j=i; j<=n; j++)
+				   System.out.print(j);  
+			}
+			System.out.println();
+		}
+	}
+}
