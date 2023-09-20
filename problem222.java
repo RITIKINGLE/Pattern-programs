504f5921facb12e10d524a8fe800465aa2719289
Ritik Ingle <78408525+RITIKINGLE@users.noreply.github.com>
2023-09-21 00:15:30 +0530

    char/L-2

---
+++ problem222.java
@@ -0,0 +1,33 @@
+/*
+EDCBABCDE
+DCBA ABCD
+CBA   ABC
+BA     AB
+A       A
+*/
+public class program222{
+	public static void main(String args[]){
+		int n=5;
+		for(int i=n; i>=1; i--)
+		{
+			if(i==n){
+				for(int j=i; j>=1;j--)
+				   System.out.print((char)(j+64));
+
+				for(int j=2; j<=i; j++)
+				   System.out.print((char)(j+64));
+			}
+			else{
+				for(int j=i; j>=1; j--)
+				   System.out.print((char)(j+64));
+
+				for(int j=1; j<=((n-i)*2)-1; j++)
+				   System.out.print(" ");
+
+				for(int j=1;  j<=i; j++)
+				   System.out.print((char)(j+64));
+			}
+			System.out.println();
+		}
+	}
+}
