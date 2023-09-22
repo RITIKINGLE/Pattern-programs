30c8dbc080166b54ca530f350a50f5d0b0738e4a
Ritik Ingle <78408525+RITIKINGLE@users.noreply.github.com>
2023-09-23 00:12:15 +0530

    L-2 char

---
+++ problem224.java
@@ -0,0 +1,33 @@
+/*
+eDcBaBcDe
+DcBa aBcD
+cBa   aBc
+Ba     aB
+a       a
+*/
+public class program224{
+	public static void main(String args[]){
+		int n=5;
+		for(int i=n; i>=1; i--)
+		{
+			if(i==n){
+				for(int j=i; j>=1;j--)
+				   System.out.print((char)(j+((j%2==0)?64:96)));
+
+				for(int j=2; j<=i; j++)
+				   System.out.print((char)(j+((j%2==0)?64:96)));
+			}
+			else{
+				for(int j=i; j>=1; j--)
+				   System.out.print((char)(j+((j%2==0)?64:96)));
+
+				for(int j=1; j<=((n-i)*2)-1; j++)
+				   System.out.print(" ");
+
+				for(int j=1;  j<=i; j++)
+				   System.out.print((char)(j+((j%2==0)?64:96)));
+			}
+			System.out.println();
+		}
+	}
+}
